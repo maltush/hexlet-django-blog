bd5a35b469d37db96b9a34f3a12b45d5b475799d
maltush <maltushw@gmail.com>
2026-05-12 09:15:42 +0300

    маршрутизация

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -2,15 +2,9 @@ from django.urls import path
 
 from hexlet_django_blog.article import views
 
-urlpatterns = [
-    path("", views.index),
-]
-
-# hexlet_django_blog/article/urls.py
-from django.urls import path
-from . import views
 
 urlpatterns = [
-    path("", views.index, name="article_index"),
+path("", views.index, name="article_index"),
+path("str:tags [blocked]/int:article\_id [blocked]/", views.index, name="article"),
 ]
 
